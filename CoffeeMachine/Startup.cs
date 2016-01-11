241940f4ed2c6f0ea83b5020a04f3e64f75b12f0
Aleksey Profatilov <snakeduse@gmail.com>
2016-01-11 22:57:18 +0300

    Add default ASP.NET project

---
+++ CoffeeMachine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoffeeMachine.Startup))]
+namespace CoffeeMachine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
